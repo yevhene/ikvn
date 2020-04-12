8740bfd162d8aecba759da1a7b627e7dbf9ec743
Yevhen Shemet <yevhene@gmail.com>
2020-04-12 16:14:33 +0300

    Refactoring

--- lib/ikvn_web/views/tournament/digest_view.ex
+++ lib/ikvn_web/views/tournament/digest_view.ex
@@ -1,13 +1,4 @@
 defmodule IkvnWeb.Tournament.DigestView do
   use IkvnWeb, :view
-  import IkvnWeb.Helpers.Markdown
-  alias Ikvn.Game.{Task, Tour}
-
-  def title(%Tour{title: title}) do
-    title || gettext("Tour")
-  end
-
-  def title(%Task{title: title}) do
-    title || gettext("Task")
-  end
+  import IkvnWeb.Helpers.{Markdown, Title}
 end
